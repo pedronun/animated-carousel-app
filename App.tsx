adf0f9a3c4e338e240de4b95242d411cb14cea9f
Pedro <pedro.lucas@econverse.com.br>
2023-10-23 18:47:40 -0300

    feat: finalizado carrossel

--- App.tsx
+++ App.tsx
@@ -1,11 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useRef } from "react";
+import {
+  Animated,
+  Dimensions,
+  Image,
+  StyleSheet,
+  View
+} from "react-native";
+const { width } = Dimensions.get("screen");
+
+const data = [
+  "https://cdn.dribbble.com/users/3281732/screenshots/11192830/media/7690704fa8f0566d572a085637dd1eee.jpg?compress=1&resize=1200x1200",
+  "https://cdn.dribbble.com/users/3281732/screenshots/13130602/media/592ccac0a949b39f058a297fd1faa38e.jpg?compress=1&resize=1200x1200",
+  "https://cdn.dribbble.com/users/3281732/screenshots/9165292/media/ccbfbce040e1941972dbc6a378c35e98.jpg?compress=1&resize=1200x1200",
+  "https://cdn.dribbble.com/users/3281732/screenshots/11205211/media/44c854b0a6e381340fbefe276e03e8e4.jpg?compress=1&resize=1200x1200",
+  "https://cdn.dribbble.com/users/3281732/screenshots/7003560/media/48d5ac3503d204751a2890ba82cc42ad.jpg?compress=1&resize=1200x1200",
+  "https://cdn.dribbble.com/users/3281732/screenshots/6727912/samji_illustrator.jpeg?compress=1&resize=1200x1200",
+  "https://cdn.dribbble.com/users/3281732/screenshots/13661330/media/1d9d3cd01504fa3f5ae5016e5ec3a313.jpg?compress=1&resize=1200x1200",
+];
+
+const imageW = width * 0.7;
+const imageH = imageW * 1.54;
 
 export default function App() {
+  const imageRef = useRef(new Animated.Value(0)).current;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
+      <View style={StyleSheet.absoluteFillObject}>
+        {data.map((item, index) => {
+          const inputRange = [
+            (index - 1) * width,
+            index * width,
+            (index + 1) * width,
+          ];
+          const opacity = imageRef.interpolate({
+            inputRange,
+            outputRange: [0, 1, 0],
+          });
+
+          return (
+            <Animated.Image
+              key={index}
+              source={{ uri: item }}
+              style={[
+                StyleSheet.absoluteFillObject,
+                {
+                  opacity,
+                },
+              ]}
+              blurRadius={14}
+            />
+          );
+        })}
+      </View>
+      <Animated.FlatList
+        data={data}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: imageRef } } }],
+          { useNativeDriver: true }
+        )}
+        keyExtractor={(_, index) => index.toString()}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        renderItem={({ item }) => {
+          return (
+            <View style={styles.itemWrapper}>
+              <Image source={{ uri: item }} style={styles.itemImage} />
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +80,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000",
+  },
+  itemWrapper: {
+    width,
+    justifyContent: "center",
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOpacity: .5,
+    shadowRadius: 20,
+  },
+  itemImage: {
+    width: imageW,
+    height: imageH,
+    resizeMode: "cover",
+    borderRadius: 16,
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
   },
 });
